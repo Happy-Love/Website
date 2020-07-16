7ee8a9ae33d6c8ab74eda909a17b55161c931751
Happy-Love <yutabox@mail.ru>
2020-07-17 01:42:11 +0300

    js update

--- js/script.js
+++ js/script.js
@@ -4,28 +4,32 @@
 // Вариант №2 использовать цикл для поиска родителя в случае если сайт будет изменяться.
 let coll=document.getElementsByClassName('collapsible');
 for (let i = 0; i < coll.length; i++) {
-   
     coll[i].addEventListener('click', function () { 
-    let parent =coll[i].parentNode;
-    let ancestor = parent.parentNode;
-    
-    // console.log(ancestor.lastChild);
-    
-    let content=ancestor.lastElementChild;
+    coll[i].previousElementSibling.classList.toggle("fa-caret-right")
+    coll[i].previousElementSibling.classList.toggle("fa-caret-down");
+    let parent=coll[i];
+    let content=coll[i];
+    while(content.className!='content' ){
+        // Если мы дошли до эншиента(корня) выходим из цикла
+        if(parent.parentNode==null)
+            break;
+        // Смотрим предыдущего    
+        parent=parent.parentNode;
+        // Cмотрим всех детей родителя и пытаемся обнаружить контент =)
+        let children=parent.children
+        for(let j = 0; j < children.length; j++)
+            if(children[j].className=='content')
+                content=children[j];
+        
+    } 
     console.log(content);
-    // for(var i=0;i<ancestor.childNodes.length;i++){
-    //     if(ancestor.childNodes[i].className=="content"){
-    //         notes=ancestor.childNodes[i];
-    //         break;
-    //     }
-    // }
     if(content.style.maxHeight)
         content.style.maxHeight=null;
     else
         content.style.maxHeight=content.scrollHeight+'px'
-    });
-    
+    }); 
 }
+
  
  
  
